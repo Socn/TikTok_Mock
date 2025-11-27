c4c4a8d1a6f08da83df9453575e89e83672b63b1
Socn <itzsocn@socn.top>
2025-11-27 15:24:17 +0000

    change collapse breakpoint to 1240px

--- src/hooks/useCollpased.tsx
+++ src/hooks/useCollpased.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 
 export default function useCollapsed() {
   const [collapsed, setCollapsed] = useState(() => {
-    return window.matchMedia('(min-width: 768px)').matches === false;
+    return window.matchMedia('(min-width: 1240px)').matches === false;
   });
 
   useEffect(() => {
     const handler = (e: MediaQueryListEvent) =>
       setCollapsed(e.matches === false);
-    window.matchMedia('(min-width: 768px)').addEventListener('change', handler);
+    window
+      .matchMedia('(min-width: 1240px)')
+      .addEventListener('change', handler);
   }, []);
 
   return collapsed;
