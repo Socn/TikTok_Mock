ac0d0606c1c2a283fd33f5ee06753b3e7101ab74
Socn <itzsocn@socn.top>
2025-11-29 15:58:57 +0000

    Rename useCollapsed.tsx to useCollapsed.ts

--- src/hooks/useCollpased.ts
+++ src/hooks/useCollpased.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
-export default function useCollapsed() {
+export default function useCollapsed(width = 1240) {
   const [collapsed, setCollapsed] = useState(() => {
-    return window.matchMedia('(min-width: 1240px)').matches === false;
+    return window.matchMedia(`(min-width: ${width}px)`).matches === false;
   });
 
   useEffect(() => {
     const handler = (e: MediaQueryListEvent) =>
       setCollapsed(e.matches === false);
     window
-      .matchMedia('(min-width: 1240px)')
+      .matchMedia(`(min-width: ${width}px)`)
       .addEventListener('change', handler);
-  }, []);
+  }, [width]);
 
   return collapsed;
 }
